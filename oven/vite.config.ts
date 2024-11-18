e16fdc3b00ba742ce8a49a6985654411d8f570a9
Anand Desai <desaianand456@gmail.com>
2024-11-18 23:48:55 +0530

    build: added new aliases, improved typescript configuration and guard configs

--- oven/vite.config.ts
+++ oven/vite.config.ts
@@ -47,6 +47,7 @@ export default defineConfig({
 			'@': path.resolve(__dirname, 'src'),
 			'@/components': path.resolve(__dirname, 'src/components'),
 			'@/utils': path.resolve(__dirname, 'src/utils'),
+			'@/stores': path.resolve(__dirname, 'src/stores'),
 			'@/api': path.resolve(__dirname, 'src/api'),
 			'@/popup': path.resolve(__dirname, 'src/popup'),
 			'@/background': path.resolve(__dirname, 'src/background'),
